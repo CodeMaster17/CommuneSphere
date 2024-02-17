dc97239a00515ffa6c74f095231ea2b8c080b184
Harshit Yadav <2105891@kiit.ac.in>
2024-02-17 20:37:49 +0530

    fix: tailwind line fixing

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,15 @@ export default function Home() {
 
 
   return (
-    <main className="w-full border-2 h-[100vh] overflow-hidden">
-      <h1 className="text-center mt-20 text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1] hidden md:block text-muted-foreground">Start managing your  <span className="text-black">team</span> <br /> with <span className="text-black">CommuneSphere</span> </h1>
-      <div className="w-full border-2 flex justify-center mt-4">
+    <main className="h-[100vh] w-full overflow-hidden border-2">
+      <h1 className="mt-20 hidden text-center text-3xl font-bold leading-tight tracking-tighter text-muted-foreground md:block md:text-6xl lg:leading-[1.1]">Start managing your  <span className="text-black">team</span> <br /> with <span className="text-black">CommuneSphere</span> </h1>
+      <div className="mt-4 flex w-full justify-center border-2">
         <Button variant="default" className="m-auto" asChild>
           <Link href="/auth/login">Login</Link>
         </Button>
       </div>
-      <div className="w-[80%] m-auto  shadow-2xl mt-4">
-        <Image src="/hero-area.jpeg" alt="intro" width={4000} height={4000} className="w-full h-full" />
+      <div className="m-auto mt-4  w-[80%] shadow-2xl">
+        <Image src="/hero-area.jpeg" alt="intro" width={4000} height={4000} className="size-full" />
       </div>
     </main>
   );
