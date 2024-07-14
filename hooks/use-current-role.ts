076727564a2ba6e199c8b1b8432a498fd192e781
Harshit Yadav <2105891@kiit.ac.in>
2024-07-14 23:25:11 +0530

    feat: created hook to fetch user

---
+++ hooks/use-current-role.ts
@@ -0,0 +1,7 @@
+// This hook is created to fetch the role of the current user
+import { useSession } from "next-auth/react";
+
+export const useCurrentRole = () => {
+  const session = useSession();
+  return session.data?.user?.role;
+};
